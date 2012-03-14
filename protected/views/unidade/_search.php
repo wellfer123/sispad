86a7adad65bbd753b43cb030be086fc69ed9be1d
cesar.consultorjr@gmail.com <>
2012-03-14 17:27:46 -0300

    views atualizadas

--- protected/views/unidade/_search.php
+++ protected/views/unidade/_search.php
@@ -11,18 +11,8 @@
 	</div>
 
 	<div class="row">
-		<?php echo $form->label($model,'descricao'); ?>
-		<?php echo $form->textField($model,'descricao',array('size'=>60,'maxlength'=>100)); ?>
-	</div>
-
-	<div class="row">
 		<?php echo $form->label($model,'nome'); ?>
 		<?php echo $form->textField($model,'nome',array('size'=>40,'maxlength'=>40)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'cidade_id'); ?>
-		<?php echo $form->textField($model,'cidade_id'); ?>
 	</div>
 
 	<div class="row buttons">
