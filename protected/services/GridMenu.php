9b7f5c5592e7e0fc13fc7c1f0124bed7976e7435
juniorpires <juniorpiresupe@gmail.com>
2012-04-13 16:49:18 -0300

    criando GridMenu

---
+++ protected/services/GridMenu.php
@@ -0,0 +1,26 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of GridMenu
+ *
+ * @author Junior Pires
+ */
+class GridMenu {
+
+    public static function menu($arrayMenu=array(),$itensLinha){
+        return "<table></table>";
+    }
+
+    private function geraItens($arrayMenu=array()){
+        foreach ($arrayMenu as $key => $value) {
+            
+        }
+
+
+    }
+}
+?>
--- protected/views/perfil/_menuCreate.php
+++ protected/views/perfil/_menuCreate.php
@@ -4,13 +4,63 @@ $this->beginWidget('zii.widgets.CPortlet', array(
 			'title'=>'Cadastro',
                         'htmlOptions'=>array('class'=>'portlet_cadastro')
 		));
-                    echo "<ul>
-                                <li>TESTE</li>
-                                <li>TESTE</li>
-                                <li>TESTE</li>
-                                <li>TESTE</li>
-                                <li>TESTE</li>
-                                
-                          </ul>";
+?>
+<table>
+    <tr>
+        <td style="text-align: center;"><?php echo CHtml::imageButton(Yii::app()->request->baseUrl.'/css/group.png',array(
+        'submit'=>'index.php?r=cidade/update&id=',
+        'alt'=>'Update')); ?><br><a href="index.php?r=cidade/update&id=">Usuarios</a></td>
+        
+        <td style="text-align: center;"><?php echo CHtml::imageButton(Yii::app()->request->baseUrl.'/css/group.png',array(
+        'submit '=>'index.php?r=cidade/update&id=',
+        'alt'=>'Update')); ?><br><a href="index.php?r=cidade/update&id=">Usuarios</a></td>
+        <td style="text-align: center;"><?php echo CHtml::imageButton(Yii::app()->request->baseUrl.'/css/group.png',array(
+        'submit'=>'index.php?r=cidade/update&id=',
+        'alt'=>'Update')); ?><br><a href="index.php?r=cidade/update&id=">Usuarios</a></td>
+
+        <td style="text-align: center;"><?php echo CHtml::imageButton(Yii::app()->request->baseUrl.'/css/group.png',array(
+        'submit '=>'index.php?r=cidade/update&id=',
+        'alt'=>'Update')); ?><br><a href="index.php?r=cidade/update&id=">Usuarios</a></td>
+        <td style="text-align: center;"><?php echo CHtml::imageButton(Yii::app()->request->baseUrl.'/css/group.png',array(
+        'submit'=>'index.php?r=cidade/update&id=',
+        'alt'=>'Update')); ?><br><a href="index.php?r=cidade/update&id=">Usuarios</a></td>
+
+        <td style="text-align: center;"><?php echo CHtml::imageButton(Yii::app()->request->baseUrl.'/css/group.png',array(
+        'submit '=>'index.php?r=cidade/update&id=',
+        'alt'=>'Update')); ?><br><a href="index.php?r=cidade/update&id=">Usuarios</a></td>
+        
+        
+    </tr>
+    <tr>
+       <td style="text-align: center;"><?php echo CHtml::imageButton(Yii::app()->request->baseUrl.'/css/group.png',array(
+        'submit'=>'index.php?r=cidade/update&id=',
+        'alt'=>'Update')); ?><br><a href="index.php?r=cidade/update&id=">Usuarios</a></td>
+
+        <td style="text-align: center;"><?php echo CHtml::imageButton(Yii::app()->request->baseUrl.'/css/group.png',array(
+        'submit '=>'index.php?r=cidade/update&id=',
+        'alt'=>'Update')); ?><br><a href="index.php?r=cidade/update&id=">Usuarios</a></td>
+        <td style="text-align: center;"><?php echo CHtml::imageButton(Yii::app()->request->baseUrl.'/css/group.png',array(
+        'submit'=>'index.php?r=cidade/update&id=',
+        'alt'=>'Update')); ?><br><a href="index.php?r=cidade/update&id=">Usuarios</a></td>
+
+        <td style="text-align: center;"><?php echo CHtml::imageButton(Yii::app()->request->baseUrl.'/css/group.png',array(
+        'submit '=>'index.php?r=cidade/update&id=',
+        'alt'=>'Update')); ?><br><a href="index.php?r=cidade/update&id=">Usuarios</a></td>
+        <td style="text-align: center;"><?php echo CHtml::imageButton(Yii::app()->request->baseUrl.'/css/group.png',array(
+        'submit'=>'index.php?r=cidade/update&id=',
+        'alt'=>'Update')); ?><br><a href="index.php?r=cidade/update&id=">Usuarios</a></td>
+
+        <td style="text-align: center;"><?php echo CHtml::imageButton(Yii::app()->request->baseUrl.'/css/group.png',array(
+        'submit '=>'index.php?r=cidade/update&id=',
+        'alt'=>'Update')); ?><br><a href="index.php?r=cidade/update&id=">Usuarios</a></td>
+    </tr>
+
+</table>
+<?php
+    
+
+
+   
+    
 $this->endWidget();
 ?>
