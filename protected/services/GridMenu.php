7bf16c5f80aa7e6ed809da535872d1ffe67ccf48
juniorpires <juniorpiresupe@gmail.com>
2012-04-16 13:58:15 -0300

    desenvolvendo o gridMenu

--- protected/services/GridMenu.php
+++ protected/services/GridMenu.php
@@ -11,12 +11,27 @@
  */
 class GridMenu {
 
-    public static function menu($arrayMenu=array(),$itensLinha){
-        return "<table></table>";
+    public static function menu($arrayMenu=array(),$options=array()){
+       echo "<table>";
+            GridMenu::geraItens($arrayMenu,$options);
+       echo "</table>";
     }
 
-    private function geraItens($arrayMenu=array()){
-        foreach ($arrayMenu as $key => $value) {
+     private static function geraItens($arrayMenu=array(),$options=array()){
+        
+        
+        while($arrayMenu!=null) {
+            echo '<tr>';
+            for($i=0;(($i<$options['columns']) && ($arrayMenu!=null));$i++){
+                echo '<td style="text-align: center;">';
+                echo CHtml::imageButton($arrayMenu[$i]['icon'],array('submit'=>$arrayMenu[$i]['link']));
+                echo "<br><a href=".$arrayMenu[$i]['link'].">".$arrayMenu[$i]['title']."</a>";
+                echo '</td>';
+                unset ($arrayMenu[$i]);
+            }
+           
+            $arrayMenu=array_values($arrayMenu);
+            echo '</tr>';
             
         }
 
--- protected/views/perfil/_menuAdmin.php
+++ protected/views/perfil/_menuAdmin.php
@@ -1,17 +1,36 @@
 <?php
+Yii::import('application.services.GridMenu');
 
 $this->beginWidget('zii.widgets.CPortlet', array(
 			'title'=>'Administracao',
                         'htmlOptions'=>array('class'=>'portlet_administracao')
 		));
-                    echo "<ul>
-                                <li>TESTE</li>
-                                <li>TESTE</li>
-                                <li>TESTE</li>
-                                <li>TESTE</li>
-                                <li>TESTE</li>
-                                
-                          </ul>";
+                GridMenu::menu(array(
+                        array('icon'=>Yii::app()->request->baseUrl.'/css/group.png',
+                              'title'=>'exemploHoje',
+                              'link'=>'http://localhost/sispad'),
+                        array('icon'=>Yii::app()->request->baseUrl.'/css/group.png',
+                              'title'=>'exemploAgora',
+                              'link'=>'sispad'),
+                        array('icon'=>Yii::app()->request->baseUrl.'/css/group.png',
+                              'title'=>'exemploHoje',
+                              'link'=>'sispad'),
+                        array('icon'=>Yii::app()->request->baseUrl.'/css/group.png',
+                              'title'=>'exemploAgora',
+                              'link'=>'sispad'),
+                        array('icon'=>Yii::app()->request->baseUrl.'/css/group.png',
+                              'title'=>'exemploHoje',
+                              'link'=>'sispad'),
+                        array('icon'=>Yii::app()->request->baseUrl.'/css/group.png',
+                              'title'=>'exemploAgora',
+                              'link'=>'sispad')
+                        
+          
+                    
+                    
+                ),array('columns'=>4));
+
+                //echo"<table><tr><td>um</td/></tr></table>";
 $this->endWidget();
 
 ?>
