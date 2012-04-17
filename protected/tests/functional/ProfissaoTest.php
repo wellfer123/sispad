c474fa9931f42eb62d21aeb80009578077a8eeb0
cesar.consultorjr@gmail.com <cesar.consultorjr@gmail.com>
2012-04-17 12:00:06 -0300

    test de profissao criados

---
+++ protected/tests/functional/ProfissaoTest.php
@@ -0,0 +1,39 @@
+<?php
+
+class ProfissaoTest extends WebTestCase
+{
+    /*
+	public $fixtures=array(
+		'profissaos'=>'Profissao',
+	);
+
+	public function testShow()
+	{
+		$this->open('?r=profissao/view&id=1');
+	}
+
+	public function testCreate()
+	{
+		$this->open('?r=profissao/create');
+	}
+
+	public function testUpdate()
+	{
+		$this->open('?r=profissao/update&id=1');
+	}
+
+	public function testDelete()
+	{
+		$this->open('?r=profissao/view&id=1');
+	}
+
+	public function testList()
+	{
+		$this->open('?r=profissao/index');
+	}
+
+	public function testAdmin()
+	{
+		$this->open('?r=profissao/admin');
+	}*/
+}
---
+++ protected/tests/unit/ProfissaoTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class ProfissaoTest extends CDbTestCase
+{
+	public $fixtures=array(
+		'profissaos'=>'Profissao',
+	);
+
+	public function testCreate()
+	{
+
+	}
+}
