2ac7028c32cb5cea17d066dfcc529fd3e8d9aa8a
cesao <cesar.consultorjr@gmail.com>
2012-06-12 16:03:59 -0300

    testes criados referentes ao modelo TecnicoEnfermagemExecutaMeta

---
+++ protected/tests/fixtures/tecnico_enfermagem.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+	/*
+	'sample1'=>array(
+		'servidor_cpf' => '',
+		'unidade_cnes' => '',
+		'data_desativacao' => '',
+		'ativo' => '',
+		'data_cadastro' => '',
+	),
+	'sample2'=>array(
+		'servidor_cpf' => '',
+		'unidade_cnes' => '',
+		'data_desativacao' => '',
+		'ativo' => '',
+		'data_cadastro' => '',
+	),
+	*/
+);
---
+++ protected/tests/unit/tecnico_enfermagem_executa_metaTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class tecnico_enfermagem_executa_metaTest extends CDbTestCase
+{
+	public $fixtures=array(
+		'tecnico_enfermagem_executa_metas'=>'tecnico_enfermagem_executa_meta',
+	);
+
+	public function testCreate()
+	{
+
+	}
+}
