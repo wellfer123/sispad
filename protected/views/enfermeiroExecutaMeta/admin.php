d77a170e75fbfad330e7192cb27827d83d583559
juniorpires <juniorpiresupe@gmail.com>
2012-06-21 14:21:02 -0300

    reenviando arquivos de view de enfermeiroExecutaMeta

--- protected/views/enfermeiroExecutaMeta/admin.php
+++ protected/views/enfermeiroExecutaMeta/admin.php
@@ -1,55 +1,66 @@
 <?php
 $this->breadcrumbs=array(
-	'Enfermeiro Executa Metas'=>array('index'),
-	'Manage',
+'Enfermeiro Executa Metas'=>array('index'),
+'Manage',
 );
 
 $this->menu=array(
-	array('label'=>'List enfermeiro_executa_meta', 'url'=>array('index')),
-	array('label'=>'Create enfermeiro_executa_meta', 'url'=>array('create')),
+array('label'=>'List enfermeiro_executa_meta', 'url'=>array('index')),
+array('label'=>'Create enfermeiro_executa_meta', 'url'=>array('create')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
 $('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
+$('.search-form').toggle();
+return false;
 });
 $('.search-form form').submit(function(){
-	$.fn.yiiGridView.update('enfermeiro-executa-meta-grid', {
-		data: $(this).serialize()
-	});
-	return false;
+$.fn.yiiGridView.update('enfermeiro-executa-meta-grid', {
+data: $(this).serialize()
+});
+return false;
 });
 ");
 ?>
 
-<h1>Manage Enfermeiro Executa Metas</h1>
+<h1>Gerenciamento Enfermeiro Executa Metas</h1>
 
 <p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
+Você pode opcionalmente entrar com um operador de comparação(<, <=, >, >=, <> ou =) iniciar cada uma de
+suas pesquisa com valores específicos de como a comparação deve ser feita.
 </p>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<?php echo CHtml::link('Pesquisa Avançada','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
 <?php $this->renderPartial('_search',array(
-	'model'=>$model,
+'model'=>$model,
 )); ?>
 </div><!-- search-form -->
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'enfermeiro-executa-meta-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		'enfermeiro_cpf',
-		'unidade_cnes',
-		'meta_id',
-		'total',
-		'data_inicio',
-		'data_fim',
-		array(
-			'class'=>'CButtonColumn',
-		),
-	),
+'id'=>'enfermeiro-executa-meta-grid',
+'dataProvider'=>$model->search(),
+'columns'=>array(
+array(
+                        'name'=>'Enfermeiro',
+                        'value'=>'$data->enfermeiro->servidor->nome',
+                ),
+                'meta.nome',
+                array(
+                        'name'=>'Valor da Meta',
+                        'value'=>'$data->meta->valor',
+                ),
+                array(
+                        'name'=>'Total de execu&ccedil;&otilde;es',
+                        'value'=>'$data->total',
+                ),
+                array(
+                        'name'=>'Status da Meta',
+                        'value'=>'$data->isMetaBatida()',
+                ),
+array(
+'class'=>'CButtonColumn',
+                        'template'=>'{view}',
+),
+),
 )); ?>
--- protected/views/enfermeiroExecutaMeta/view.php
+++ protected/views/enfermeiroExecutaMeta/view.php
@@ -1,28 +1,56 @@
 <?php
 $this->breadcrumbs=array(
-	'Enfermeiro Executa Metas'=>array('index'),
-	$model->enfermeiro_cpf,
+'Enfermeiro Executa Metas'=>array('index'),
+$model->enfermeiro_cpf,
 );
 
 $this->menu=array(
-	array('label'=>'List enfermeiro_executa_meta', 'url'=>array('index')),
-	array('label'=>'Create enfermeiro_executa_meta', 'url'=>array('create')),
-	array('label'=>'Update enfermeiro_executa_meta', 'url'=>array('update', 'id'=>$model->enfermeiro_cpf)),
-	array('label'=>'Delete enfermeiro_executa_meta', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->enfermeiro_cpf),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage enfermeiro_executa_meta', 'url'=>array('admin')),
+array('label'=>'List enfermeiro_executa_meta', 'url'=>array('index')),
+array('label'=>'Create enfermeiro_executa_meta', 'url'=>array('create')),
+array('label'=>'Update enfermeiro_executa_meta', 'url'=>array('update', 'id'=>$model->enfermeiro_cpf)),
+array('label'=>'Delete enfermeiro_executa_meta', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->enfermeiro_cpf),'confirm'=>'Are you sure you want to delete this item?')),
+array('label'=>'Manage enfermeiro_executa_meta', 'url'=>array('admin')),
 );
 ?>
 
-<h1>View enfermeiro_executa_meta #<?php echo $model->enfermeiro_cpf; ?></h1>
+
+
+<div class="update">
+<h3>Meta: <?php echo $model->meta->nome.' executada por '.$model->enfermeiro->servidor->nome; ?></h3>
+</div>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'enfermeiro_cpf',
-		'unidade_cnes',
-		'meta_id',
-		'total',
-		'data_inicio',
-		'data_fim',
-	),
+'data'=>$model,
+'attributes'=>array(
+array(
+                        'label'=>'Indicador',
+                        'value'=>$model->meta->indicador->nome
+                ),
+                array(
+                        'label'=>'Meta',
+                        'value'=>$model->meta->nome
+                ),
+                array(
+                        'label'=>'Unidade',
+                        'value'=>$model->enfermeiro->unidade->nome
+                ),
+                array(
+                        'label'=>'Médico',
+                        'value'=>$model->enfermeiro->servidor->nome
+                ),
+                array(
+                        'label'=>'Valor da meta',
+                        'value'=>$model->meta->valor
+                ),
+                array(
+                        'label'=>'Total de execuções',
+                        'value'=>$model->total
+                ),
+                array(
+                        'label'=>'Status da Meta',
+                        'value'=>$model->isMetaBatida(),
+                ),
+'data_inicio',
+'data_fim',
+),
 )); ?>
