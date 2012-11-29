c221248e135365a7692c33706974994a7dc53bc7
juniorpires <juniorpiresupe@gmail.com>
2012-11-29 20:39:12 -0300

    resolvendo pendencias

--- protected/config/main.php
+++ protected/config/main.php
@@ -111,7 +111,7 @@ return array(
 		*/
 		//169.254.18.175
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=sispad',
+			'connectionString' => 'mysql:host=192.168.0.155;dbname=sispad',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
