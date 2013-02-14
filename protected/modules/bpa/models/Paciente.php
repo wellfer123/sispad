7063bd0c97cae98af1a26943a7d8bc46816e7f98
cesao <cesar.consultorjr@gmail.com>
2013-02-14 17:37:20 -0300

    bug de exclusao de todos os pacientes sem cns consertado.

--- protected/modules/bpa/models/Paciente.php
+++ protected/modules/bpa/models/Paciente.php
@@ -16,50 +16,56 @@
  * @property string $data_cadastro
  * @property integer $id
  */
-class Paciente extends CActiveRecord
-{
-        /**
-         * @var string número do cns
-         * @soap
-         */ 
-        public $cns;
-        /**
-         * @var string nome
-         * @soap
-         */ 
-         public $nome;
-         /**
-         * @var string sexo
-         * @soap
-         */ 
-         public $sexo;
-         /**
-         * @var date data de nascimento
-         * @soap
-         */ 
-         public $data_nascimento;
-         /**
-         * @var string código IBGE da cidade
-         * @soap
-         */ 
-         public $cidade;
-         /**
-         * @var string código da nacionalidade
-         * @soap
-         */ 
-         public $nacionalidade;
-         /**
-         * @var string raça
-         * @soap
-         */ 
-         public $raca;
-         /**
-         * @var string etnia, caso seja de raça indígena
-         * @soap
-         */ 
-         public $etnia;    
-    
-    
+class Paciente extends CActiveRecord {
+
+    /**
+     * @var string número do cns
+     * @soap
+     */
+    public $cns;
+
+    /**
+     * @var string nome
+     * @soap
+     */
+    public $nome;
+
+    /**
+     * @var string sexo
+     * @soap
+     */
+    public $sexo;
+
+    /**
+     * @var date data de nascimento
+     * @soap
+     */
+    public $data_nascimento;
+
+    /**
+     * @var string código IBGE da cidade
+     * @soap
+     */
+    public $cidade;
+
+    /**
+     * @var string código da nacionalidade
+     * @soap
+     */
+    public $nacionalidade;
+
+    /**
+     * @var string raça
+     * @soap
+     */
+    public $raca;
+
+    /**
+     * @var string etnia, caso seja de raça indígena
+     * @soap
+     */
+    public $etnia;
+
 //         function __construct($paciente=null) {
 //             if($paciente!= null){
 //                 $this->cidade=$paciente->cidade;
@@ -73,157 +79,152 @@ class Paciente extends CActiveRecord
 //             }
 //             
 //         }
-         
-         public function setPaciente($paciente){
-             
-                 $this->cidade=$paciente->cidade;
-                 $this->cns=$paciente->cns;
-                 $this->data_nascimento=$paciente->data_nascimento;
-                 $this->etnia=$paciente->etnia;
-                 $this->nacionalidade=$paciente->nacionalidade;
-                 $this->nome=$paciente->nome;
-                 $this->raca=$paciente->raca;
-                 $this->sexo=$paciente->sexo;
-         }
-         
-         
-         /**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return Paciente the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'bpa_paciente';
-	}
+    public function setPaciente($paciente) {
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('nome, sexo, data_nascimento, cidade, nacionalidade,  raca, ultima_atualizacao', 'required'),
-			array('cns', 'length', 'max'=>15),
-			array('nome', 'length', 'max'=>30),
-			array('sexo', 'length', 'max'=>1),
-			array('cidade', 'length', 'max'=>6),
-			array('nacionalidade', 'length', 'max'=>3),
-			array('raca', 'length', 'max'=>2),
-			array('etnia', 'length', 'max'=>4),
-                        array('data_cadastro,id,etnia', 'safe'),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('cns, nome, sexo, data_nascimento, cidade, nacionalidade,  raca, etnia, ultima_atualizacao, data_cadastro', 'safe', 'on'=>'search'),
-		);
-	}
+        $this->cidade = $paciente->cidade;
+        $this->cns = $paciente->cns;
+        $this->data_nascimento = $paciente->data_nascimento;
+        $this->etnia = $paciente->etnia;
+        $this->nacionalidade = $paciente->nacionalidade;
+        $this->nome = $paciente->nome;
+        $this->raca = $paciente->raca;
+        $this->sexo = $paciente->sexo;
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return Paciente the static model class
+     */
+    public static function model($className=__CLASS__) {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'cns' => 'Cns',
-			'nome' => 'Nome',
-			'sexo' => 'Sexo',
-			'data_nascimento' => 'Data Nascimento',
-			'cidade' => 'Cidade',
-			'nacionalidade' => 'Nacionalidade',
-			'raca' => 'Raca',
-			'etnia' => 'Etnia',
-			'ultima_atualizacao' => 'Ultima Atualizacao',
-			'data_cadastro' => 'Data Cadastro',
-                        'id' => 'ID',
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return 'bpa_paciente';
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('nome, sexo, data_nascimento, cidade, nacionalidade,  raca, ultima_atualizacao', 'required'),
+            array('cns', 'length', 'max' => 15),
+            array('nome', 'length', 'max' => 30),
+            array('sexo', 'length', 'max' => 1),
+            array('cidade', 'length', 'max' => 6),
+            array('nacionalidade', 'length', 'max' => 3),
+            array('raca', 'length', 'max' => 2),
+            array('etnia', 'length', 'max' => 4),
+            array('data_cadastro,id,etnia', 'safe'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('cns, nome, sexo, data_nascimento, cidade, nacionalidade,  raca, etnia, ultima_atualizacao, data_cadastro', 'safe', 'on' => 'search'),
+        );
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+        );
+    }
 
-		$criteria->compare('cns',$this->cns,true);
-		$criteria->compare('nome',$this->nome,true);
-		$criteria->compare('sexo',$this->sexo,true);
-		$criteria->compare('data_nascimento',$this->data_nascimento,true);
-		$criteria->compare('cidade',$this->cidade,true);
-		$criteria->compare('nacionalidade',$this->nacionalidade,true);
-		$criteria->compare('raca',$this->raca,true);
-		$criteria->compare('etnia',$this->etnia,true);
-		$criteria->compare('ultima_atualizacao',$this->ultima_atualizacao,true);
-		$criteria->compare('data_cadastro',$this->data_cadastro,true);
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'cns' => 'Cns',
+            'nome' => 'Nome',
+            'sexo' => 'Sexo',
+            'data_nascimento' => 'Data Nascimento',
+            'cidade' => 'Cidade',
+            'nacionalidade' => 'Nacionalidade',
+            'raca' => 'Raca',
+            'etnia' => 'Etnia',
+            'ultima_atualizacao' => 'Ultima Atualizacao',
+            'data_cadastro' => 'Data Cadastro',
+            'id' => 'ID',
+        );
+    }
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
-        
-        public function getCnsNome(){
-            return $this->cns.'/'.$this->nome;
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search() {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
+
+        $criteria = new CDbCriteria;
+
+        $criteria->compare('cns', $this->cns, true);
+        $criteria->compare('nome', $this->nome, true);
+        $criteria->compare('sexo', $this->sexo, true);
+        $criteria->compare('data_nascimento', $this->data_nascimento, true);
+        $criteria->compare('cidade', $this->cidade, true);
+        $criteria->compare('nacionalidade', $this->nacionalidade, true);
+        $criteria->compare('raca', $this->raca, true);
+        $criteria->compare('etnia', $this->etnia, true);
+        $criteria->compare('ultima_atualizacao', $this->ultima_atualizacao, true);
+        $criteria->compare('data_cadastro', $this->data_cadastro, true);
+
+        return new CActiveDataProvider($this, array(
+                    'criteria' => $criteria,
+                ));
+    }
+
+    public function getCnsNome() {
+        return $this->cns . '/' . $this->nome;
+    }
+
+    /**
+     * Deleta os apcientes de acordo com os ids passados
+     * @param array $ids todos os pacientes que deve ser deletados
+     */
+    public static function deleteAllNovosESemCNS($ids) {
+        $sql = 'DELETE FROM bpa_paciente WHERE (cns IS NULL) AND id IN(';
+        $cont = 0;
+        foreach ($ids as $key => $value) {
+            if ($cont === 0) {
+                $sql = $sql . ':' . $key;
+                $cont++;
+            } else {
+                $sql = $sql . ',:' . $key;
+            }
         }
-        /**
-         * Deleta os apcientes de acordo com os ids passados
-         * @param array $ids todos os pacientes que deve ser deletados
-         */
-        public static function deleteAllNovosESemCNS($ids){
-            $sql='DELETE FROM bpa_paciente WHERE (cns IS NULL) AND id IN(';
-            $cont=0;
+        $sql = $sql . ');';
+
+        $con = Yii::app()->db;
+        $transc = $con->beginTransaction();
+        try {
+            $command = $con->createCommand($sql);
+            //bind os parâmetros
             foreach ($ids as $key => $value) {
-                if ( $cont !== 0){
-                $sql=$sql.',:'.$key;
-                }
-                else{
-                  $sql=$sql.':'.$key;  
-                }
+
+                $command->bindParam(':' . $key, $value, PDO::PARAM_INT);
             }
-            $sql=$sql.');';
-            
-            $con=Yii::app()->db;
-            $transc=$con->beginTransaction();
-            try{
-                $command=$con->createCommand($sql);
-                //bind os parâmetros
-                foreach ($ids as $key => $value) {
-                    
-                $command->bindParam(':'.$key, $value, PDO::PARAM_INT);
-                }
-                $command->execute();
-                $transc->commit();
-            }catch(Exception $ex){
-                $transc->rollback();
-                Yii::log($ex->getMessage(), CLogger::LEVEL_ERROR);
-            }
+            $command->execute();
+            $transc->commit();
+        } catch (Exception $ex) {
+            $transc->rollback();
+            Yii::log($ex->getMessage(), CLogger::LEVEL_ERROR);
         }
-        public function __toString() {
-            return $this->cns;
-        }
+    }
+
+    public function __toString() {
+        return $this->cns;
+    }
 
 }
