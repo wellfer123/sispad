b7de1ed900c35db18ffd41bc18cdec50c79d80f2
Cesar Albuquerque <cesar.consultorjr@gmail.com>
2013-04-08 15:20:09 -0300

    Criada view view que exibe os dados completos de uma producao.

--- protected/views/producaoDiaria/view.php
+++ protected/views/producaoDiaria/view.php
@@ -3,28 +3,40 @@
 /* @var $model ProducaoDiaria */
 
 $this->breadcrumbs=array(
-	'Producao Diarias'=>array('index'),
-	$model->unidade_cnes,
+	'Producao Diária',
+	'Visualização',
 );
 
-$this->menu=array(
-	array('label'=>'List ProducaoDiaria', 'url'=>array('index')),
-	array('label'=>'Create ProducaoDiaria', 'url'=>array('create')),
-	array('label'=>'Update ProducaoDiaria', 'url'=>array('update', 'id'=>$model->unidade_cnes)),
-	array('label'=>'Delete ProducaoDiaria', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->unidade_cnes),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage ProducaoDiaria', 'url'=>array('admin')),
-);
+
 ?>
-
-<h1>View ProducaoDiaria #<?php echo $model->unidade_cnes; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
-		'unidade_cnes',
-		'servidor_cpf',
-		'profissao_codigo',
+                array(
+                    'label'=>'Unidade',
+                    'value' => $model->unidade->nome,
+                ),
+                array(
+                    'label'=>'Gestor',
+                    'value' => $model->gestor->nome,
+                ),
+                array(
+                    'label'=>'Profissional',
+                    'value' => $model->profissional->nome,
+                ),
+                array(
+                    'label'=>'Especialidade',
+                    'value' => $model->especialidade->nome,
+                ),
+                array(
+                    'label'=>'Grupo',
+                    'value' => $model->grupo->nome,
+                ),
 		'quantidade',
-		'data',
+                array(
+                    'label'=>'Grupo',
+                    'value' => ParserDate::inverteDataEnToPt($model->data),
+                )
 	),
 )); ?>
