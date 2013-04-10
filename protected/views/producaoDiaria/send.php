b2a743507cbd83798f91f15108074a21c31d1c7c
Cesar Albuquerque <cesar.consultorjr@gmail.com>
2013-04-10 10:27:45 -0300

    consertado o bug do link ver unidade na view send de producaoDiaria.

--- protected/views/producaoDiaria/send.php
+++ protected/views/producaoDiaria/send.php
@@ -8,7 +8,6 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'Ver Unidade', 'url'=>array('unidade/view','id'=>123)),
         array('label'=>'Histórico Produção Diária', 'url'=>array('producaoDiaria/adminGestor')),
         array('label'=>'Vincular Profissional', 'url'=>array('profissionalVinculo/create')),
 );
