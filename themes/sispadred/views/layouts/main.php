15711665ed346352c8b7afeb070fff2ab4b4182d
Cesar Albuquerque <cesar.consultorjr@gmail.com>
2013-04-26 09:49:33 -0300

    novos menus adicionados: gestor e gerencia da unidade. Retirado os menus que nao estavam sendo utilizados.

--- themes/sispadred/views/layouts/main.php
+++ themes/sispadred/views/layouts/main.php
@@ -24,75 +24,68 @@
         <div class="container" id="page">
 
             <div id="header">
-                <div id="logo"><?php //echo CHtml::encode(Yii::app()->name);  ?></div>
+                <div id="logo"><?php //echo CHtml::encode(Yii::app()->name);    ?></div>
             </div><!-- header -->
 
 
             <div id="mainmenu2">
                 <?php
-                //instancia do RBAC para verificação de acesso ao menu
-                $RBAC2 = new RBACAccessVerifier();
                 //SOMENTE O SUPER ADMINISTRADOR PODE VER O MENU COMPLETO
-                $visible = $RBAC2->checkAccess('SuperAdmin');
-                
-              
-                //$roles = $this->getState("roles");
-                 $gestor_visible= false;
-                 //pega todos os papeis daquele profissional
-                 $roles =  Yii::app()->user->getState(UserIdentity::ROLES);
-                 if($roles!=null){
-                    foreach($roles as $n=>$role)
-                    {
-                            //verifica se ele possui o papel de gestor
-                            if($n==UserIdentity::ROLE_GESTOR){
-                                $gestor_visible = true;
-
-                            }
+                $admin_visible = false;
+                $gestor_visible = false;
+                $gerencia_unidade_visible = false;
+                //pega todos os papeis daquele profissional
+                $roles = Yii::app()->user->getState(UserIdentity::ROLES);
+                if ($roles != null) {
+                    foreach ($roles as $n => $role) {
+                        //verifica os papéis que o usuário possui e exibi os menus
+                        if ($n == UserIdentity::GERENTE_UNIDADE) {
+                            $gerencia_unidade_visible = true;
+                        } elseif ($n == UserIdentity::ADMIN) {
+                            $admin_visible = true;
+                        } elseif ($n == UserIdentity::GESTOR) {
+                            $gestor_visible = true;
+                        }
                     }
-                 }
-                $menuGestor=array();
-                
-                //$servidor=$this->getServidor();
-                $menuGestor=array("url" => array(),
-                            "label" => "Gestor",
-                            array("url" => array("route" => "/producaoDiaria/send"), "label" => "Enviar Produção Diária"),
-                            array("url" => array("route" => "/producaoDiaria/adminGestor"), "label" => "Consultar Histórico"),
-                            array("url" => array("route" => "/profissionalVinculo/create"), "label" => "Vincular Profissional"),
-                            'visible' =>$gestor_visible,
-                    );
+                }
                 
                 $this->widget('application.extensions.menu.SMenu', array(
                     "menu" => array(
                         //menu raiz
                         array("url" => array("route" => "/"), "label" => "Início"),
-//                        array("url" => array(),
-//                            "label" => "Relatório",
-//                            array("url" => array("route" => "/Relatorio/create"), "label" => "Enviar Meu Relatório"),
-//                            array("url" => array("route" => "/TotalRelatorio/create"), "label" => "Enviar Quantidade"),
-//                            array("url" => array("route" => "/TotalRelatorio/list"), "label" => "Total de Relatórios (Meus)"),
-//                            'visible' => $visible,
-//                        ),
-                        $menuGestor,
-//                        array("url" => array(),
-//                            "label" => "Faltas",
-//                            array("url" => array("route" => "/Falta/preparedCreate"), "label" => "Enviar"),
-//                            array("url" => array("route" => "/Falta/preparedViewDetail"), "label" => "Relatorio Detalhado"),
-//                            array("url" => array("route" => "/Falta/preparedViewMonth"), "label" => "Relatorio Mensal"),
-//                            'visible' => $visible,
-//                        ),
+                        //menu do gerente da unidade
+                        array("url" => array(),
+                            "label" => "G. da Unidade",
+                            array("url" => array("route" => "/producaoDiaria/send"), "label" => "Enviar Produção Diária"),
+                            array("url" => array("route" => "/producaoDiaria/adminGestor"), "label" => "Consultar Histórico"),
+                            array("url" => array("route" => "/profissionalVinculo/create"), "label" => "Vincular Profissional"),
+                            'visible' => $gerencia_unidade_visible,
+                        ),
+                        //menu para os gestores
+                        array("url" => array(),
+                            "label" => "Gestor",
+                            array("url" => array("route" => "/producaoDiaria/admin"), "label" => "Consultar Histórico"),
+                            array("url" => array("route" => ""), "label" => "Relatórios da Produção Diária",
+                                array("url" => array("route" => "/producaoDiaria/monthEspecialidade"), "label" => "Por Especialidade"),
+                                array("url" => array("route" => "/producaoDiaria/monthEspecialidadeGrupo"), "label" => "Por Especialidade e Grupo"),
+                                array("url" => array("route" => "/producaoDiaria/monthGrupo"), "label" => "Por Grupo"),
+                                array("url" => array("route" => "/producaoDiaria/monthProfissional"), "label" => "Por Profissional"),
+                            ),
+                            'visible' => $gestor_visible,
+                        ),
+                        //menus que somente são e
                         array("url" => array(),
                             "label" => "Cadastro",
                             array("url" => array("route" => "/cargo/create"), "label" => "Cargo"),
-                            array("url" => array("route" => "/grupo/create"), "label" => "Grupo"),
                             array("url" => array("route" => "/competencia/create"), "label" => "Competências"),
                             array("url" => array("route" => "/departamento/create"), "label" => "Departamento"),
-                            array("url" => array("route" => "/Setor/create"), "label" => "Setor"),
-                            array("url" => array("route" => "/Servidor/create"), "label" => "Servidor"),
-                            array("url" => array("route" => "/unidadeGestor/create"), "label" => "Gestor de Unidade"),
-                            array("url" => array("route" => "/Unidade/create"), "label" => "Unidade"),
-                            array("url" => array("route" => "/metas/indicador/create"), "label" => "Indicador"),
                             array("url" => array("route" => "/metas/equipe/create"), "label" => "Equipe"),
-                            'visible' => $visible,
+                            array("url" => array("route" => "/unidadeGestor/create"), "label" => "Gerentes de Unidades"),
+                            array("url" => array("route" => "/grupo/create"), "label" => "Grupo"),
+                            array("url" => array("route" => "/Servidor/create"), "label" => "Servidor"),
+                            array("url" => array("route" => "/Setor/create"), "label" => "Setor"),
+                            array("url" => array("route" => "/Unidade/create"), "label" => "Unidade"),
+                            'visible' => $admin_visible,
                         ),
                         array("url" => array(),
                             "label" => "Administração",
@@ -100,22 +93,9 @@
                             array("url" => array("route" => "/competencia/admin"), "label" => "Competências"),
                             array("url" => array("route" => "/user/admin"), "label" => "Usuário"),
                             array("url" => array("route" => "/rbac/assignment"), "label" => "Permissões"),
-                            array("url" => array("route" => "/metas/indicador/admin"), "label" => "Indicadores"),
-                            array("url" => array("route" => "/metas/equipe/admin"), "label" => "Equipes"),
                             array("url" => array("route" => "/producaoDiaria/adminSuper"), "label" => "Produção Diária"),
                             array("url" => array("route" => "/profissionalVinculo/admin"), "label" => "Vínculo do Servidor"),
-                            array("url" => array("route" => ""), "label" => "Metas Executadas",
-                                array("url" => array("route" => "/metas/medicoExecutaMeta/admin"), "label" => "Executadas por Médico"),
-                                array("url" => array("route" => "/metas/enfermeiroExecutaMeta/admin"), "label" => "Enfermeiro"),
-                                array("url" => array("route" => "/metas/odontologoExecutaMeta/admin"), "label" => "Odontologo"),
-                                array("url" => array("route" => "/metas/agenteSaudeExecutaMeta/admin"), "label" => "Agente de Saúde"),
-                            ),
-                            array("url" => array("route" => ""), "label" => "Metas",
-                                array("url" => array("route" => "/metas/meta/calculo"), "label" => "Lançamento"),
-                            ),
-                            array("url" => array("route" => "relatorio/admin"), "label" => "Relatórios"),
-                            array("url" => array("route" => "totalRelatorio/admin"), "label" => "Quantidade de Relatórios"),
-                            'visible' => $visible,
+                            'visible' => $admin_visible,
                         ),
                         //menu raiz
                         array("url" => array(),
@@ -123,7 +103,11 @@
                             array("url" => array("route" => "/site/page/about", "view" => "about"), "label" => "Sobre"),
                             array("url" => array("route" => "/site/contact"), "label" => "Contato")),
                         array("url" => array("route" => "user/login"), "label" => "Entrar", "visible" => Yii::app()->user->isGuest),
-                        array("url" => array("route" => "user/logout"), "label" => 'Sair (' . Yii::app()->user->name . ')', "visible" => !Yii::app()->user->isGuest),
+                        array("url" => array(),
+                            "label" => Yii::app()->user->name,
+                            array("url" => array("route" => "user/logout"), "label" => "Sair"),
+                            'visible' => !Yii::app()->user->isGuest,
+                        ),
                     ),
                     "stylesheet" => "menudropdown.css", //Yii::app()->request->baseUrl.'/css/menudropdown.css'
                     "menuID" => "myMenu",
@@ -132,22 +116,13 @@
                 );
                 ?>
                 <p></p>
-                <?php /* $this->widget('zii.widgets.CMenu',array(
-                  'items'=>array(
-                  array('label'=>'Home', 'url'=>array('/site/index')),
-                  array('label'=>'Sobre', 'url'=>array('/site/page', 'view'=>'about')),
-                  array('label'=>'Contato', 'url'=>array('/site/contact')),
-                  array('label'=>'Entrar', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-                  array('label'=>'Sair ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-                  ),
-                  )); */ ?>
             </div> <!-- mainmenu -->
-                <?php if (isset($this->breadcrumbs)): ?>
-                    <?php
-                    $this->widget('zii.widgets.CBreadcrumbs', array(
-                        'links' => $this->breadcrumbs,
-                    ));
-                    ?><!-- breadcrumbs -->
+            <?php if (isset($this->breadcrumbs)): ?>
+                <?php
+                $this->widget('zii.widgets.CBreadcrumbs', array(
+                    'links' => $this->breadcrumbs,
+                ));
+                ?><!-- breadcrumbs -->
             <?php endif ?>
 
             <?php echo $content; ?>
